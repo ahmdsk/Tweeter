2e97ec74914e53d678d1d2e4a63697a14e186c5a
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-20 15:00:36 +0700

    Update Dropdown Menu Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 import { AiFillCaretDown } from "react-icons/ai"
+import { FaUserCircle, FaUserFriends }  from "react-icons/fa"
+import { MdSettings } from "react-icons/md"
+import { TbLogout } from "react-icons/tb"
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 export default function Header() {
     return (
@@ -24,11 +29,57 @@ export default function Header() {
                 </ul>
             </div>
             <div className="w3/12">
-                <div className="flex gap-3 items-center justify-between">
-                    <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Profile" className="w-10 rounded-lg" />
-                    <h3 className="text-gray-800 font-semibold text-base">Xanthe Neal</h3>
-                    <AiFillCaretDown />
-                </div>
+                <Menu as="div" className="relative">
+                    <Menu.Button className="flex gap-3 items-center justify-between">
+                        <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Profile" className="w-10 rounded-lg" />
+                        <h3 className="text-gray-700 font-semibold text-base">Xanthe Neal</h3>
+                        <AiFillCaretDown />
+                    </Menu.Button>
+
+                    <Menu.Items className="absolute top-16 right-0 bg-white flex flex-col border-2 rounded-xl py-3 px-4 shadow-md w-64">
+                        <Menu.Item className="flex items-center gap-3 px-4 py-3 rounded-xl">
+                            {({ active }) => (
+                                <a
+                                    className={`text-gray-700 font-medium ${active && 'bg-gray-100'}`}
+                                    href="/account-settings"
+                                ><FaUserCircle className="text-xl" />
+                                    My Profile
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item className="flex items-center gap-3 px-4 py-3 rounded-xl">
+                            {({ active }) => (
+                                <a
+                                    className={`text-gray-700 font-medium ${active && 'bg-gray-100'}`}
+                                    href="/account-settings"
+                                ><FaUserFriends className="text-xl" />
+                                    Group Chat
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item className="flex items-center gap-3 px-4 py-3 rounded-xl">
+                            {({ active }) => (
+                                <a
+                                    className={`text-gray-700 font-medium ${active && 'bg-gray-100'}`}
+                                    href="/account-settings"
+                                ><MdSettings className="text-xl" />
+                                    Settings
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <div className="border-b-2 my-2"></div>
+                        <Menu.Item className="flex items-center gap-3 px-4 py-3 rounded-xl">
+                            {({ active }) => (
+                                <a
+                                    className={`text-red-500 font-medium ${active && 'bg-gray-100'}`}
+                                    href="/account-settings"
+                                ><TbLogout className="text-xl" />
+                                    Logout
+                                </a>
+                            )}
+                        </Menu.Item>
+                    </Menu.Items>
+                </Menu>
             </div>
         </header>
     )
