3877ae2dafe4f8171bdb117f9fea816dad19ffbd
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-21 14:20:08 +0700

    Refactoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
+import App from './pages/App'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <App />
+  },
+  {
+    path: '/explore',
+    element: <App />
+  },
+  {
+    path: '/bookmarks',
     element: <App />
   }
 ])
---
+++ src/pages/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import HomeSection from "../components/Home/HomeSection"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomeSection />
+      <Footer />
+    </>
+  )
+}
+
+export default App
