a897493e88cc254fcd5be634abb3e46a8a0e3ba3
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-22 11:53:50 +0700

    Fix Footer Hidden

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import BootomBar from "./BootomBar"
 export default function Footer() {
   return (
     <>
-      <div className="w-full text-center py-4">
+      <div className="w-full text-center pt-4 pb-24">
           <h3 className="text-gray-400">Created by <a href="https://github.com/ahmdsk" className="font-bold border-b-2">ahdmsk</a> - devChallenges.io</h3>
       </div>
       
