750c899f985bdfb0d5da9d638b3b3bd5438e9336
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-22 15:39:22 +0700

    Responsive Profile

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -3,31 +3,36 @@ import { FaUserPlus } from "react-icons/fa"
 export default function ProfileCard() {
     return (
         <div className="bg-white lg:mx-28 md:mx-6 py-6 md:py-3 px-8 md:px-4 min-[300px]:mx-3 rounded-lg mb-4">
-            <div className="flex">
-                <div className="w-2/12 relative">
-                    <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Profile" className="w-36 rounded-lg absolute bottom-10 md:bottom-16 left-0 border border-4" />
+            <div className="flex md:flex-row min-[300px]:flex-col min-[300px]:items-center">
+                <div className="md:w-2/12 min-[300px]:w-full relative min-[300px]:flex justify-center">
+                    <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Profile" className="md:w-36 min-[300px]:w-24 rounded-lg absolute md:bottom-2 md:left-0 border min-[300px]:bottom-0 border-4" />
                 </div>
                 <div className="w-10/12">
-                    <div className="flex flex-col justify-between gap-6 md:gap-3 md:px-4">
-                        <div className="flex justify-between items-center">
-                            <div className="flex items-center gap-8 md:gap-4">
+                    <div className="flex flex-col justify-between md:items-stretch gap-6 md:gap-3 min-[300px]:gap-3 md:px-4 min-[300px]:items-center">
+                        <div className="flex md:flex-row min-[300px]:flex-col justify-between items-center">
+                            <div className="flex md:flex-row min-[300px]:flex-col items-center md:gap-4 min-[300px]:gap-2">
                                 <h3 className="text-2xl md:text-xl font-semibold text-gray-700">Ahmad Ganteng</h3>
-                                <div className="flex gap-2">
-                                    <p className="text-gray-700 font-semibold md:text-sm">2,569</p>
-                                    <p className="text-gray-400 md:text-sm">Following</p>
-                                </div>
-                                <div className="flex gap-2">
-                                    <p className="text-gray-700 font-semibold md:text-sm">10.8K</p>
-                                    <p className="text-gray-400 md:text-sm">Followers</p>
+                                <div className="flex gap-3 md:pb-0 min-[300px]:pb-2 min-[300px]:justify-between">
+                                    <div className="flex gap-2">
+                                        <p className="text-gray-700 font-semibold md:text-sm">2,569</p>
+                                        <p className="text-gray-400 md:text-sm">Following</p>
+                                    </div>
+                                    <div className="flex gap-2">
+                                        <p className="text-gray-700 font-semibold md:text-sm">10.8K</p>
+                                        <p className="text-gray-400 md:text-sm">Followers</p>
+                                    </div>
                                 </div>
                             </div>
-                            <button className="bg-blue-500 text-white font-medium py-2 px-6 rounded hover:bg-blue-800 flex items-center gap-2">
+                            <button className="bg-blue-500 text-white font-medium py-2 px-6 rounded hover:bg-blue-800 md:flex items-center gap-2 min-[300px]:hidden">
                                 <FaUserPlus className="font-semibold" /> Follow
                             </button>
                         </div>
-                        <div className="w-3/4 h-20">
+                        <div className="w-3/4 h-20 min-[300px]:h-auto min-[300px]:text-center md:text-start min-[300px]:w-full">
                             <p className="text-gray-400 font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         </div>
+                        <button className="bg-blue-500 text-white font-medium p-2 px-6 rounded hover:bg-blue-800 min-[300px]:flex items-center gap-2 md:hidden">
+                            <FaUserPlus className="font-semibold" /> Follow
+                        </button>
                     </div>
                 </div>
             </div>
--- src/components/Profile/ProfileSection.jsx
+++ src/components/Profile/ProfileSection.jsx
@@ -9,11 +9,11 @@ export default function ProfileSection() {
         <div className="bg-[url('https://images.unsplash.com/photo-1676763134010-5aefbe59285b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80')] w-full h-72 bg-no-repeat bg-cover bg-center pt-48">
             <ProfileCard />
 
-            <div className="lg:mx-28 md:mx-6 flex items-start gap-5">
-                <div className="w-4/12">
+            <div className="lg:mx-28 md:mx-6 flex md:flex-row items-start gap-5 min-[300px]:flex-col min-[300px]:px-3">
+                <div className="md:w-4/12 min-[300px]:w-full">
                     <LeftMenu menu={menu} />
                 </div>
-                <div className="w-8/12">
+                <div className="md:w-8/12 min-[300px]:w-full">
                     <TweetSection />
                 </div>
             </div>
