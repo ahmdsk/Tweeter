a172bbc77d2c13a52f0dc22ae858585465a56876
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-23 11:17:36 +0700

    add transition bottom bar

--- src/components/BootomBar.jsx
+++ src/components/BootomBar.jsx
@@ -7,13 +7,13 @@ export default function BootomBar() {
 
     return (
         <div className="bg-white flex items-center justify-around fixed py-2 px-4 gap-5 bottom-0 w-full md:hidden">
-            <NavLink to="/" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition-all">
+            <NavLink to="/" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition ease-in delay-150">
                 <AiFillHome className="text-gray-600 text-2xl" />
             </NavLink>
-            <NavLink to="/explore" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition-all">
+            <NavLink to="/explore" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition ease-in delay-150">
                 <MdExplore className="text-gray-600 text-2xl" />
             </NavLink>
-            <NavLink to="/bookmarks" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition-all">
+            <NavLink to="/bookmarks" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition ease-in delay-150">
                 <BsBookmarkFill className="text-gray-600 text-2xl" />
             </NavLink>
         </div>
