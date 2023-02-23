423358a61cea84b7dd02e464a1f8fa2f980cfd25
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-23 11:14:55 +0700

    fix hover bottom bar

--- src/components/BootomBar.jsx
+++ src/components/BootomBar.jsx
@@ -4,19 +4,17 @@ import { BsBookmarkFill } from "react-icons/bs"
 import { NavLink } from "react-router-dom"
 
 export default function BootomBar() {
-    let disableLink = 'rounded-lg w-4/12 py-2 px-3 flex justify-center'
-    let activeLink = `bg-gray-200 ${disableLink}`
 
     return (
         <div className="bg-white flex items-center justify-around fixed py-2 px-4 gap-5 bottom-0 w-full md:hidden">
-            <NavLink to="/" className={({isActive}) => isActive ? activeLink : disableLink}>
+            <NavLink to="/" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition-all">
                 <AiFillHome className="text-gray-600 text-2xl" />
             </NavLink>
-            <NavLink to="/explore" className={({isActive}) => isActive ? activeLink : disableLink}>
+            <NavLink to="/explore" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition-all">
                 <MdExplore className="text-gray-600 text-2xl" />
             </NavLink>
-            <NavLink to="/bookmarks" className={({isActive}) => isActive ? activeLink : disableLink}>
-                <BsBookmarkFill className="text-gray-600 text-xl" />
+            <NavLink to="/bookmarks" className="rounded-lg w-4/12 py-2 px-3 flex justify-center hover:bg-gray-200 transition-all">
+                <BsBookmarkFill className="text-gray-600 text-2xl" />
             </NavLink>
         </div>
     )
